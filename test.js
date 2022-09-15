46bc457661bdfe97b4774409dbd1a481dde99347
Chen <jonkiky@gmail.com>
2022-09-15 09:25:11 -0400

    update style

--- test.js
+++ test.js
@@ -4,19 +4,18 @@ var vCardsJS = require('vcards-js');
 var vCard = vCardsJS();
  
 //set properties
-vCard.firstName = 'Eric';
-vCard.middleName = 'J';
-vCard.lastName = 'Nesser';
-vCard.organization = 'ACME Corporation';
-vCard.photo.attachFromUrl('https://avatars2.githubusercontent.com/u/5659221?v=3&s=460', 'JPEG');
-vCard.workPhone = '312-555-1212';
-vCard.birthday = new Date(1985, 0, 1);
-vCard.title = 'Software Developer';
-vCard.url = 'https://github.com/enesser';
-vCard.note = 'Notes on Eric';
+vCard.firstName = 'Yizhen';
+vCard.lastName = 'Chen';
+vCard.uid = '8319d07d-385f-4091-9309-5fcda62234bc';
+vCard.organization = 'National Cancer Institute';
+vCard.photo.attachFromUrl('https://raw.githubusercontent.com/jonkiky/home/main/img/me.jpg', 'JPEG');
+vCard.workPhone = '518-256-3656';
+vCard.title = 'Senior Software Developer';
+vCard.email = 'ychen315040@gmail.com';
+vCard.workEmail = 'yizhen.chen@nih.gov';
  
 //save to file
-vCard.saveToFile('./eric-nesser.vcf');
+vCard.saveToFile('./yizhenchen.vcf');
  
 //get as formatted string
 console.log(vCard.getFormattedString());
